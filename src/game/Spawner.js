f30c78c1485d2af6851e26e93fe2aced4fcf588a
Satya Reddy <kingadheerafansatya@gmail.com>
2026-07-03 23:30:36 +0530

    Update Spawner.js

--- src/game/Spawner.js
+++ src/game/Spawner.js
@@ -1,4 +1,3 @@
-// src/game/Spawner.js
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
 import { CONFIG } from '../config/GameConfig.js';
 import { Boss } from '../entities/Boss.js';
